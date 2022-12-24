d28b15c718f10dcacfbe651bf73295d5e5dc3ac8
Jeho Lee <wpghrkwkd@gmail.com>
2022-12-24 14:43:01 +0900

    first upload

---
+++ OlineBookStoreMain.java
@@ -0,0 +1,12 @@
+package com.mystudy.olinebookstore;
+
+public class OlineBookStoreMain {
+
+	public static void main(String[] args) {
+
+		OlineBookStore book = new OlineBookStore();
+		book.start();
+		
+
+	} 
+}
